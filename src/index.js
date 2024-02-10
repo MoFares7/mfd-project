2f7a39b4f0fd39673b643091fabdedd9c9f35442
MoFares7 <faresdabbas1@gmail.com>
2024-02-10 17:00:07 +0300

    create crud api in express js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ const express = require("express");
 const app = express();
 const PORT = '3001';
 
+app.use(express.json());
+
 app.listen(PORT, () => console.log('Running Express server in Port : ' + PORT));
 
+//define list 
+const listTesting = [{
+        name: "fares",
+        quantity: 4
+}];
+
 app.get('/books', (req, res) => {
-        res.send([
-                {
-                        "name": "tesing in software",
-                        "number": 203
-                },
-                {
-                        "name": "tesing in software",
-                        "number": 203
-                },
-                {
-                        "name": "tesing in software",
-                        "number": 203
-                },
-        ])
+        res.send(listTesting)
+        return
+});
+
+app.post('/books', (req, res) => {
+        listTesting.push(req.body);
+        res.status(201).send(listTesting);
+        return
 })
